d56f263578b955c2236bf50b8d3151ad5bacf532
remy-rabischung <161297446+remy-rabischung@users.noreply.github.com>
2024-03-07 15:59:03 +0100

    Add files via upload

---
+++ Jour3/Job2/main.py
@@ -0,0 +1,5 @@
+def My_print_name():
+    name = "Remy"
+    print(name)
+
+My_print_name()
---
+++ Jour3/Job5/main.py
@@ -0,0 +1,7 @@
+def calcule():
+    num1 = 10
+    num2 = 5
+    operator = num1 * num2
+    print (operator)
+
+calcule()
